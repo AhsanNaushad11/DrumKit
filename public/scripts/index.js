3ad16e00601cb421d4b5effa49b56c93d1fc40a9
ahsannaushad11 <ahsannaushadkarim@gmail.com>
2022-09-13 12:01:25 +0500

    Implemented keystrokes

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -14,45 +14,55 @@ let numberOfDrumButtons = drums.length;
 for (let i = 0; i < numberOfDrumButtons; i++) {
   //   Better way to assign functions to the objects is in javascript, not HTML.
   drums[i].addEventListener("click", function () {
-    switch (this.innerHTML) {
-      case "w":
-        var audio = new Audio("./sounds/tom-1.mp3");
-        audio.play();
-        break;
-
-      case "a":
-        var audio = new Audio("./sounds/tom-2.mp3");
-        audio.play();
-        break;
-
-      case "s":
-        var audio = new Audio("./sounds/tom-3.mp3");
-        audio.play();
-        break;
-
-      case "d":
-        var audio = new Audio("./sounds/tom-4.mp3");
-        audio.play();
-        break;
-
-      case "j":
-        var audio = new Audio("./sounds/snare.mp3");
-        audio.play();
-        break;
-
-      case "k":
-        var audio = new Audio("./sounds/crash.mp3");
-        audio.play();
-        break;
-
-      case "l":
-        var audio = new Audio("./sounds/kick-bass.mp3");
-        audio.play();
-        break;
-
-      default:
-        console.log("Invalid key");
-        break;
-    }
+    stroke(this.innerHTML); //  Object's inner HTML will be passed.
+    buttonAnimation(this.innerHTML);    //  Highlighting whatever is pressed.
   });
+}
+
+document.addEventListener("keydown", function (event) {
+  stroke(event.key); //  whatever key is pressed its string value will be passed.
+  buttonAnimation(event.key);
+});
+
+function stroke(keyInput) {
+  switch (keyInput) {
+    case "w":
+      var audio = new Audio("./sounds/tom-1.mp3");
+      audio.play();
+      break;
+
+    case "a":
+      var audio = new Audio("./sounds/tom-2.mp3");
+      audio.play();
+      break;
+
+    case "s":
+      var audio = new Audio("./sounds/tom-3.mp3");
+      audio.play();
+      break;
+
+    case "d":
+      var audio = new Audio("./sounds/tom-4.mp3");
+      audio.play();
+      break;
+
+    case "j":
+      var audio = new Audio("./sounds/snare.mp3");
+      audio.play();
+      break;
+
+    case "k":
+      var audio = new Audio("./sounds/crash.mp3");
+      audio.play();
+      break;
+
+    case "l":
+      var audio = new Audio("./sounds/kick-bass.mp3");
+      audio.play();
+      break;
+
+    default:
+      console.log("Invalid key");
+      break;
+  }
 }
